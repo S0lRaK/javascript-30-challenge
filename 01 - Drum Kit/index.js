77a68ee2318dcdb1ded4bfaefaade4f12db731b6
S0lRaK <carlosj.gc989@gmail.com>
2021-04-02 20:30:13 -0600

    feat: :sparkles: Drum Kit

---
+++ 01 - Drum Kit/index.js
@@ -0,0 +1,22 @@
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.key}"]`)
+  if (!audio) return // stop function run if there is no audio-key association
+  audio.currentTime = 0 // set audio to start
+  audio.play()
+}
+
+function addStyleClass(e) {
+  const key = document.querySelector(`.keys__key[data-key="${e.key}"`)
+  key.classList.toggle('keys__key_playing')
+}
+
+function removeStyleClass(e) {
+  if (e.propertyName !== 'transform') return
+  this.classList.remove('keys__key_playing')
+}
+
+document.addEventListener('keydown', playSound)
+document.addEventListener('keydown', addStyleClass)
+
+const keys = document.querySelectorAll('.keys__key')
+keys.forEach((key) => key.addEventListener('transitionend', removeStyleClass))

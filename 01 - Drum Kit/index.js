60db4a4c4fd8c2afe002e9b841e0f24c59a7fd7b
S0lRaK <carlosj.gc989@gmail.com>
2021-04-05 10:25:38 -0600

    refactor: :recycle: Anonymous functions

--- 01 - Drum Kit/index.js
+++ 01 - Drum Kit/index.js
@@ -1,15 +1,16 @@
-function playSound(e) {
+const playSound = (e) => {
   const audio = document.querySelector(`audio[data-key="${e.key}"]`)
   if (!audio) return // stop function run if there is no audio-key association
   audio.currentTime = 0 // set audio to start
   audio.play()
 }
 
-function addStyleClass(e) {
+const addStyleClass = (e) => {
   const key = document.querySelector(`.keys__key[data-key="${e.key}"`)
   key.classList.toggle('keys__key_playing')
 }
 
+// cannot be anonymous func because this then comes from parent scope (Window)
 function removeStyleClass(e) {
   if (e.propertyName !== 'transform') return
   this.classList.remove('keys__key_playing')

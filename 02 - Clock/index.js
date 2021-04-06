fd4f26091504a11df1ca3d879fa2b30b351ff466
S0lRaK <carlosj.gc989@gmail.com>
2021-04-06 16:16:14 -0600

    fix: :bug: Toggle transition to avoid seconds' hand going backwards

--- 02 - Clock/index.js
+++ 02 - Clock/index.js
@@ -1,5 +1,6 @@
 const SECONDS_IN_A_MIN = 60
 const MINS_IN_AN_HOUR = 60
+const HOURS_IN_12_HOUR_CLOCK = 12
 const CIRCLE_DEGREES = 360
 const OFFSET_ROTATION_DEGREES = 90 // hand starts horizontal -> 0º
 
@@ -18,6 +19,13 @@ const setClock = (now = new Date()) => {
   const seconds = now.getSeconds()
   const secondsDegrees =
     (seconds / SECONDS_IN_A_MIN) * CIRCLE_DEGREES + OFFSET_ROTATION_DEGREES
+
+  if (seconds === 0) {
+    handSecond.classList.toggle('hand--no-transition')
+  } else if (seconds === 1) {
+    handSecond.classList.toggle('hand--no-transition')
+  }
+
   handSecond.style.transform = `rotate(${secondsDegrees}deg)`
 
   const minutes = now.getMinutes()
@@ -29,9 +37,9 @@ const setClock = (now = new Date()) => {
 
   const hours = now.getHours()
   const hoursDegrees =
-    (hours / 12) * CIRCLE_DEGREES +
+    (hours / HOURS_IN_12_HOUR_CLOCK) * CIRCLE_DEGREES +
     OFFSET_ROTATION_DEGREES +
-    (minutesDegrees - OFFSET_ROTATION_DEGREES) / 12
+    (minutesDegrees - OFFSET_ROTATION_DEGREES) / HOURS_IN_12_HOUR_CLOCK
   handHour.style.transform = `rotate(${hoursDegrees}deg)`
 
   playSound()
